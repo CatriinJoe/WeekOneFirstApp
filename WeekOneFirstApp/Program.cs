d8263e2e0461a6231867514d142250e048ba9946
Cätriin Jõe <catriin.joe@tktk.ee>
2024-09-16 16:06:12 +0300

    add files

--- WeekOneFirstApp/Program.cs
+++ WeekOneFirstApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is a comment
+
+string myName = "Cätriin";
+string myLastName = "Jõe";
+
+//Console.WriteLine("Hello" + " " + myName");
+
+Console.WriteLine($"Hello, {myName} {myLastName}!"); 
